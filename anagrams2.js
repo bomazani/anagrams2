fdb61b1ebce4a3193152ac962721b9caaa1b48f6
Bob Hunt <bob.hunt.email@gmail.com>
2018-03-20 15:07:42 -0400

    Its not working.

--- anagrams2.js
+++ anagrams2.js
@@ -1,11 +1,15 @@
-function alphabetize(word) {
-    return word.toLowerCase().split("").sort().join("").trim();
+
+
+// var typedText = document.getElementById("input").value;
+
+function alphabetize(typedText) {
+    return word.toLowerCase().split("").sort().join("").trim();   
 }
 
 // adds function to the user button //
 document.getElementById("findButton").onclick = function() {
     let typedText = document.getElementById("input").value;
-    
+    console.log(typedText);
     for (i=0; i<words.length; i++){
         let dictionary = words[i];
         
@@ -16,14 +20,31 @@ document.getElementById("findButton").onclick = function() {
             }
        
     }  
-}   
-      
+} 
+function getAnagrams(word){
+
+    const anagrams = {};
+    for (let i=0; i<words.length; i++){
+        const word = words[i];
+        const alphaWord = alphabetize(word);
+        if (alphaWord in anagrams){
+            anagrams[alphaWord].push(word);
+        }else{
+            anagrams[alphaWord]=[word];
+        }
+    }
         
+    return anagrams;
+    const foundAnagrams = getAnagrams();
+    for(let key in foundAnagrams){
+        console.log(key);
+        console.log(foundAnagrams[key]);
+        if(foundAnagrams[key].length>=5){
+            console.log(foundAnagrams[key]);
+        }
+    }
+}   
 
-
-
-
-// End test //
 
 
 
