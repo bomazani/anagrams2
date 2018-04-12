9ce66bb82a8051a440a2a2a0227267dcb6714b79
Bob Hunt <bob.hunt.email@gmail.com>
2018-04-12 17:15:53 -0400

    fixed link to library

--- anagrams2.js
+++ anagrams2.js
@@ -3,47 +3,47 @@
 // var typedText = document.getElementById("input").value;
 
 function alphabetize(typedText) {
-    return word.toLowerCase().split("").sort().join("").trim();   
+    return word.toLowerCase().split("").sort().join("").trim();
 }
 
 // adds function to the user button //
-document.getElementById("findButton").onclick = function() {
+document.getElementById("findButton").onclick = function () {
     let typedText = document.getElementById("input").value;
     console.log(typedText);
-    for (i=0; i<words.length; i++){
+    for (i = 0; i < words.length; i++) {
         let dictionary = words[i];
-        
-        if(alphabetize(typedText) === alphabetize(dictionary)) {
-                
+
+        if (alphabetize(typedText) === alphabetize(dictionary)) {
+
             document.write((dictionary) + ",  ");
 
-            }
-       
-    }  
-} 
-function getAnagrams(word){
+        }
+
+    }
+}
+function getAnagrams(word) {
 
     const anagrams = {};
-    for (let i=0; i<words.length; i++){
+    for (let i = 0; i < words.length; i++) {
         const word = words[i];
         const alphaWord = alphabetize(word);
-        if (alphaWord in anagrams){
+        if (alphaWord in anagrams) {
             anagrams[alphaWord].push(word);
-        }else{
-            anagrams[alphaWord]=[word];
+        } else {
+            anagrams[alphaWord] = [word];
         }
     }
-        
+
     return anagrams;
     const foundAnagrams = getAnagrams();
-    for(let key in foundAnagrams){
+    for (let key in foundAnagrams) {
         console.log(key);
         console.log(foundAnagrams[key]);
-        if(foundAnagrams[key].length>=5){
+        if (foundAnagrams[key].length >= 5) {
             console.log(foundAnagrams[key]);
         }
     }
-}   
+}
 
 
 
